dc4503690381f8ad6aa966c13b57b3e384838824
Michael <sciencekid13@gmail.com>
2019-11-02 14:02:47 -0400

    Added Chat Violations to /report, also fixed some of the languages used in messages! :)

--- src/main/java/net/excentrix/core/Commands/report.java
+++ src/main/java/net/excentrix/core/Commands/report.java
@@ -16,29 +16,32 @@ public class report implements CommandExecutor {
                         Player target = Bukkit.getPlayerExact(args[0]);
                         if (target != null) {
                             if (args[1].equalsIgnoreCase("KillAura") || args[1].equalsIgnoreCase("ka")) {
-                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.GOLD + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "KillAura" + ChatColor.GREEN + "!");
+                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.YELLOW + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "KillAura" + ChatColor.GREEN + "!");
                                 notifyStaff(((Player) sender).getDisplayName(), target.getDisplayName(), args);
                             } else if (args[1].equalsIgnoreCase("aimbot")) {
-                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.GOLD + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "Aimbot" + ChatColor.GREEN + "!");
+                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.YELLOW + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "Aimbot" + ChatColor.GREEN + "!");
                                 notifyStaff(((Player) sender).getDisplayName(), target.getDisplayName(), args);
                             } else if (args[1].equalsIgnoreCase("Fly") || args[1].equalsIgnoreCase("flight")) {
-                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.GOLD + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "Flight" + ChatColor.GREEN + "!");
+                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.YELLOW + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "Flight" + ChatColor.GREEN + "!");
                                 notifyStaff(((Player) sender).getDisplayName(), target.getDisplayName(), args);
                             } else if (args[1].equalsIgnoreCase("antikb") || args[1].equalsIgnoreCase("antiknockback") || args[1].equalsIgnoreCase("knockback") || args[1].equalsIgnoreCase("velocity") || args[1].equalsIgnoreCase("anti-knockback")) {
-                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.GOLD + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "Anti-Knockback" + ChatColor.GREEN + "!");
+                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.YELLOW + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "Anti-Knockback" + ChatColor.GREEN + "!");
                                 notifyStaff(((Player) sender).getDisplayName(), target.getDisplayName(), args);
                             } else if (args[1].equalsIgnoreCase("crits") || args[1].equalsIgnoreCase("criticals") || args[1].equalsIgnoreCase("crit")) {
-                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.GOLD + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "Criticals" + ChatColor.GREEN + "!");
+                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.YELLOW + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "Criticals" + ChatColor.GREEN + "!");
                                 notifyStaff(((Player) sender).getDisplayName(), target.getDisplayName(), args);
                             } else if (args[1].equalsIgnoreCase("bhop") || args[1].equalsIgnoreCase("BunnyHop") || args[1].equalsIgnoreCase("B-Hop")) {
-                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.GOLD + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "BunnyHop" + ChatColor.GREEN + "!");
+                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.YELLOW + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "BunnyHop" + ChatColor.GREEN + "!");
                                 notifyStaff(((Player) sender).getDisplayName(), target.getDisplayName(), args);
                             } else if (args[1].equalsIgnoreCase("speed") || args[1].equalsIgnoreCase("movement")) {
-                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.GOLD + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "Speed" + ChatColor.GREEN + "!");
+                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.YELLOW + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.RED + "Speed" + ChatColor.GREEN + "!");
+                                notifyStaff(((Player) sender).getDisplayName(), target.getDisplayName(), args);
+                            } else if (args[1].equalsIgnoreCase("chat") || args[1].equalsIgnoreCase("spam") || args[1].equalsIgnoreCase("advertising") || args[1].equalsIgnoreCase("spamming")) {
+                                sender.sendMessage(ChatColor.GREEN + "Thanks for reporting " + ChatColor.YELLOW + target.getDisplayName() + ChatColor.GREEN + " for " + ChatColor.YELLOW + "Chat Violations" + ChatColor.GREEN + "!");
                                 notifyStaff(((Player) sender).getDisplayName(), target.getDisplayName(), args);
                             } else {
                                 sender.sendMessage(ChatColor.GREEN + "Sorry, I couldn't verify what you're reporting.");
-                                sender.sendMessage(ChatColor.RED + "Valid Hacks: (KILLAURA, AIMBOT, FLIGHT, ANTIKNOCKBACK, CRITICALS, BHOP, SPEED)");
+                                sender.sendMessage(ChatColor.RED + "Valid Hacks: (KILLAURA, AIMBOT, FLIGHT, ANTIKNOCKBACK, CRITICALS, BHOP, SPEED, SPAM)");
                             }
                         } else {
                             sender.sendMessage(ChatColor.RED + "There is no player by that name connected to this server!");
@@ -55,9 +58,9 @@ public class report implements CommandExecutor {
         for (final Player staff : Bukkit.getOnlinePlayers()) {
             if (staff.hasPermission("excentrix.chat.staffchat")) {
                 if (args[1].equalsIgnoreCase("KA")) {
-                    staff_utils.scNotif("Console", staff, ChatColor.GRAY + sender + ChatColor.AQUA + " has reported " + ChatColor.GOLD + target + ChatColor.AQUA + " for " + ChatColor.RED + "KillAura");
+                    staff_utils.scNotif("Console", staff, ChatColor.GRAY + sender + ChatColor.AQUA + " has reported " + ChatColor.YELLOW + target + ChatColor.AQUA + " for " + ChatColor.RED + "KillAura");
                 } else
-                    staff_utils.scNotif("Console", staff, ChatColor.GRAY + sender + ChatColor.AQUA + " has reported " + ChatColor.GOLD + target + ChatColor.AQUA + " for " + ChatColor.RED + args[1].toUpperCase());
+                    staff_utils.scNotif("Console", staff, ChatColor.GRAY + sender + ChatColor.AQUA + " has reported " + ChatColor.YELLOW + target + ChatColor.AQUA + " for " + ChatColor.RED + args[1].toUpperCase());
             }
         }
     }
